d2517d167fa4f568c05e9136f4cdc75a36e92405
Adam Hemming <hemming.dev@gmail.com>
2022-09-23 20:33:29 +0900

    create files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import GeneralInfo from "./Components/GeneralInfo";
+import EducationInfo from "./Components/EducationInfo";
+import ExperienceInfo from "./Components/ExperienceInfo";
 
 function App() {
   return (
