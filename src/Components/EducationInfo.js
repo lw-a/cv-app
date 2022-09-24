1c34dacec1d7d5c2c68e66ba892a7404436121a9
Adam Hemming <hemming.dev@gmail.com>
2022-09-24 19:49:56 +0900

    save and edit education section

--- src/Components/EducationInfo.js
+++ src/Components/EducationInfo.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
+function UserDetails(props) {
+	const { school, type, from, to, handleClick } = props;
+	return (
+		<div>
+			<p><strong>School Name:</strong> {school}</p>
+			<p><strong>Type of Study:</strong> {type}</p>
+			<p><strong>from:</strong> {from}</p>
+			<p><strong>to:</strong> {to}</p>
+			<button onClick={handleClick}>Edit</button>
+		</div>
+	);
+}
+
 class EducationInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      editMode: true,
       school: '',
       type: '',
       from: '',
       to: '',
     }
-
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -20,21 +34,37 @@ class EducationInfo extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
+    this.setState((prevState) => {
+      return {editMode: !prevState.editMode}
+    });
   }
 
   render() {
+    const { editMode, school, type, from, to } = this.state;
+
+		if (!editMode) {
+			return (
+				<UserDetails
+					school={school}
+					type={type}
+					from={from}
+					to={to}
+					handleClick={this.handleSubmit}
+				/>
+			);
+		}
     return (
       <div>
         <h2>Education</h2>
         <form onSubmit={this.handleSubmit}>
-          <input type='text' placeholder='School Name' name='school' onChange={this.handleChange} value={this.state.school} />
+          <input type='text' placeholder='School Name' name='school' onChange={this.handleChange} value={school} />
 
-          <input type='text' placeholder='Type of Study' name='type' onChange={this.handleChange} value={this.state.type} />
+          <input type='text' placeholder='Type of Study' name='type' onChange={this.handleChange} value={type} />
 
-          <input type='date' name='from' onChange={this.handleChange} value={this.state.from} />
+          <input type='date' name='from' onChange={this.handleChange} value={from} />
 
-          <input type='date' name='to' onChange={this.handleChange} value={this.state.to} />
-          <button>Save</button>
+          <input type='date' name='to' onChange={this.handleChange} value={to} />
+          <button type='submit'>Save</button>
         </form>
       </div>
     )
